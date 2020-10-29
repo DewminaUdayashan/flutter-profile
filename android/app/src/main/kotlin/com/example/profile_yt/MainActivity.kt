82ea3b39042eabb298edce7055c35498d72c095d
DewminaUdayashan <dewminaudayashan@gmail.com>
2020-10-29 12:20:38 +0530

    flutter profile

---
+++ android/app/src/main/kotlin/com/example/profile_yt/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.profile_yt
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import 'ProfileScreen.dart';
+
+void main() => runApp(
+      DevicePreview(
+        enabled: !kReleaseMode,
+        builder: (context) => MyApp(),
+      ),
+    );
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ProfileScreen(),
+    );
+  }
+}
